feb1b2dd904854e690229295b9ad71d274082e0c
Egbune-github <111042283+Egbune-github@users.noreply.github.com>
2022-09-19 10:39:38 +0100

    Pointer

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,20 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _strlen - Returns the length of a string
- * @s: String to count
- *
- * Return: String length
- * int _strlen(char *s)
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
 {
-int c = 0;
+	int c = 0;
 
-for (; *s != '\0'; s++)
-{
-c++;
-}
-return (c);
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
 }
 
